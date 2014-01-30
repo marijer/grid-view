8cf6bf9476a2433e4faaba9e9b9f2328a4b98100
Marije Rooze <marijer@gmail.com>
2014-01-30 13:19:23 +0100

    set sections next to each other. Introduced keyboard actions

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,93 +1,156 @@
 $( document ).ready(function() {
 
-
 	var Navigation =  {
-	numColumns: 0,
-	numRows: 2,
-	currentVertical: 0,
-	currentHorizontal: 0,
+		numColumns: 2,
+		numRows: 3,
+		posVertical: 1, // set starting pos
+		posHorizontal: 1,
 
-	init: function() {
-		// activate nav items that are clickable
-		Navigation.checkConstraints();
+		init: function() {
+			// activate nav items that are clickable
+			this.checkConstraints();
 
-		// makes sure we always start in the left top corner
-		$("html, body").animate({ scrollTop: 0 }, "slow");
+			// makes sure we always start in the left top corner
+			this.moveTo();
+		},
 
-		console.log('hi');
-	},
+		checkConstraints: function(){
+			var $up =  $('#up'),
+				 $down = $('#down'),
+				 $left = $('#left'),
+				 $right = $('#right');
 
-	checkConstraints: function(){
-		var $up =  $('#up'),
-		$down = $('#down'),
-		$left = $('#left'),
-		$right = $('#right');
+			if ( this.posVertical <= 1 ) {
+				$up.addClass('inactive');
+			} else if( $up.hasClass('inactive') ){
+				$up.removeClass('inactive')
+			} 
 
-		if ( this.currentVertical <= 0 ) {
-			$up.addClass('inactive');
-		} else if( $up.hasClass('inactive') ){
-			$up.removeClass('inactive')
-		} 
+			if ( this.posVertical >= this.numRows ){
+				$down.addClass('inactive');
+			}else if ($down.hasClass('inactive') ) {
+				$down.removeClass('inactive');
+			}
 
-		if ( this.currentVertical >= this.numRows ){
-			$down.addClass('inactive');
-		}else if ($down.hasClass('inactive') ) {
-			$down.removeClass('inactive');
+			if ( this.posHorizontal <= 1 ) {
+				$left.addClass('inactive');
+			} else if( $left.hasClass('inactive') ){
+				$left.removeClass('inactive')
+			} 
+
+			if ( this.posHorizontal >= this.numColumns ) {
+				$right.addClass('inactive');
+			} else if( $right.hasClass('inactive' ) ){
+				$right.removeClass('inactive')
+			} 		
+		},
+
+		update: function() {
+			console.log("v: " + Navigation.posVertical + " h: " + Navigation.posHorizontal);
+
+			var target = $("section[data-column='" + Navigation.posHorizontal +"']").find("article[data-slide-id='" + Navigation.posVertical +"']");
+			if ( target.length === 0 ) return false;
+
+			var prevSlide = $('article.active')[0] || false;
+			$(prevSlide).removeClass('active');
+
+			Navigation.moveTo();
+		},
+
+		onClick: function( e ) {
+			var that = this;
+			e.preventDefault();
+
+			Navigation.updatePos(e.target.id);
+			Navigation.update();
+		},
+
+		onKeyDown: function( e ) {
+			var keyCode = e.keyCode,
+				 pressed;
+
+			switch (keyCode){
+				case 37:
+					pressed = "left";
+					break;
+				case 38:
+					pressed = "up";
+					break;
+				case 39:
+					pressed = "right";
+					break;
+				case 40: 
+					pressed = "down";
+					break;
+				default:
+					// some other key code we do not care about
+			}	 
+
+			Navigation.updatePos(pressed);
+			Navigation.update();
+		},
+
+		moveTo: function(  ){
+
+			var target = $("section[data-column='" + this.posHorizontal +"']").find("article[data-slide-id='" + this.posVertical +"']");
+
+			target.addClass('active');
+			$('body').animate({ 
+							scrollTop: target.offset().top, 
+							scrollLeft: target.offset().left, 
+						},'slow');
+		},
+
+		updatePos: function( direction ) {
+			switch( direction ) {
+				case 'down':
+					if (this.posVertical < this.numRows) this.posVertical++;
+					break;
+				case 'up':
+					if (this.posVertical !== 0 ) this.posVertical--;
+					break;
+				case 'left':
+					if (this.posHorizontal !== 0 ) this.posHorizontal--;
+					break;
+				case 'right':
+					if (this.posHorizontal < this.numColumns ) this.posHorizontal++;
+					break;
+				default:
+				console.log('Update Movement: doing nothing');
+				break;
+			}
+			Navigation.checkConstraints();
 		}
+	}
 
-		if ( this.currentHorizontal <= 0 ) {
-			$left.addClass('inactive');
-		} else if( $left.hasClass('inactive') ){
-			$left.removeClass('inactive')
-		} 
+	var Content = {
+		update: function() {
 
-		if ( this.currentHorizontal >= this.numColumns ) {
-			$right.addClass('inactive');
-		} else if( $right.hasClass('inactive' ) ){
-			$right.removeClass('inactive')
-		} 		
-	},
-
-	onclick: function( e ) {
-		e.preventDefault();
-
-		var prevSlide = $('article.active')[0] || false;
-		this.currentVertical = prevSlide.getAttribute('data-slide-id');
-		$(prevSlide).removeClass('active');
-
-		Navigation.updateMovement(e.target.id);
-		
-		Navigation.moveTo();
-	},
-
-	moveTo: function (  ){
-		var target = $("article[data-slide-id='" + this.currentVertical +"']") || false;
-		if ( !target ) return;
-		$(target).addClass('active');
-		$('body').animate({ scrollTop: target.offset().top },'slow');
-	},
-
-	updateMovement: function( el ) {
-		switch( el ) {
-			case 'down':
-			if (this.currentVertical <= this.numRows) this.currentVertical++;
-			break;
-			case 'up':
-			if (this.currentVertical !== 0 ) this.currentVertical--;
-			break;
-			default:
-			console.log('doing nothing');
-			break;
 		}
+	}
 
-		Navigation.checkConstraints();
-	}
-	}
+	var nav = document.querySelector('nav');
+	nav.addEventListener('click', Navigation.onClick);
+
+	document.addEventListener("keydown", Navigation.onKeyDown, false);
+
 
 	Navigation.init();
-	var nav = document.querySelector('nav');
-	nav.addEventListener('click', Navigation.onclick);
 
+
+ // $(window).resize(function() {
+ //      updateView();
+ //  });
+
+setWindowSizes()
+
+	
+	 function setWindowSizes(){
+	 	   var window_width = $(window).width();
+	      var window_height = $(window).height();
+	      $('section').css('width', window_width + "px");
+	      $('section').css('height', window_height + "px");
+	 }
 
 }); // on end ready
 
